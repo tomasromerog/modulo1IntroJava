7f5e310534c476f5d5ad0ad20edd5b7641ef589e
tomi <tomas.tromero@mercadolibre.com>
2021-05-26 15:28:05 -0300

    Subiendo arraylist

---
+++ Pruebas/src/demo/DemoArrayList.java
@@ -0,0 +1,28 @@
+package demo;
+
+import java.util.ArrayList;
+
+public class DemoArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("Tomas");
+        a.add("Pedro");
+        a.add("Juan");
+
+        for(int i = 0; i < a.size() ; i++){
+            System.out.println(a.get(i));
+        }
+
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        b.add(1);
+        b.add(2);
+        b.add(3);
+        Integer c = 4;
+        b.add(c);
+
+        for(int i = 0; i < b.size() ; i++){
+            System.out.println(b.get(i));
+        }
+    }
+}
--- Pruebas/src/demo/NumerosNaturales.java
+++ Pruebas/src/demo/NumerosNaturales.java
@@ -3,25 +3,37 @@ package demo;
 import java.util.Scanner;
 
 public class NumerosNaturales {
-    public static boolean esNatural(int numero){
-        int contador = 0;
-        boolean natural = true;
-
-        while (contador != numero){
-            contador++;
-        }
-        return natural;
-    }
 
     public static void main(String[] args) {
-        Scanner introduceNumber = new Scanner(System.in);
-        System.out.println("Ingrese un valor");
-        int number = (int) introduceNumber.nextDouble();
+        Scanner number = new Scanner(System.in);
 
-        for(int i = 1; i <= number; i++){
-            if(esNatural(i))
-                System.out.println(i + " numeros naturales");
+        System.out.println("Ingrese la cantidad de numeros a imprimir");
+        int cantidadImprimir = Integer.parseInt(number.nextLine());
+
+        System.out.println("Ingrese la cantidad minima de digitos a buscar en el numero: ");
+        int minimoBuscar = Integer.parseInt(number.nextLine());
+
+        System.out.println("ingrese el digito a buscar: " );
+        int digitoBuscar = Integer.parseInt(number.nextLine());
+
+        int cantidad = 0;
+        int numero = digitoBuscar;
+
+        while(cantidad < cantidadImprimir){
+            int digitosEncontrados = 0;
+            int numeroIt = numero;
+
+            while(numeroIt > 0){
+                if(numeroIt % 10 == digitoBuscar){
+                    ++digitosEncontrados;
+                }
+                numeroIt = numeroIt/10;
+            }
+            if( digitosEncontrados == minimoBuscar){
+                System.out.println(numero);
+                ++cantidad;
+            }
+            numero++;
         }
-        introduceNumber.close();
     }
 }
